f016608b917bfc2f8fe7074422483a3d32752dd6
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2020-12-29 08:17:49 +0800

    2020.12.29

--- main_5050.py
+++ main_5050.py
@@ -78,14 +78,14 @@ for k in range(times):
         knn = KNeighborsClassifier(n_neighbors=num_neig)
         knn.fit(Xtr_all[i][:, optimizer.X_alpha.astype(bool)], ytr_all[i])
         
-        acc = np.round(accuracy_score(knn.predict(Xts_all[i][:, optimizer.X_alpha.astype(bool)]), yts_all[i]), 2)
+        acc = accuracy_score(knn.predict(Xts_all[i][:, optimizer.X_alpha.astype(bool)]), yts_all[i])
         selected = np.sum(optimizer.X_alpha)
-        loss = w1*acc + w2*selected/len(optimizer.X_alpha)
+        loss = w1*(1-acc) + w2*selected/len(optimizer.X_alpha)
         
         table[0, i] += selected # for mean selected
         table[1, i] += acc # for mean acc
         table[2, i] += loss # for mean loss
-        table[6, i] += round(time.time()-start, 2) # for mean time
+        table[6, i] += time.time()-start # for mean time
         
         tmep_for_std[k, i] = loss
         tmep_for_loss[:, i] += optimizer.gBest_curve
@@ -99,9 +99,12 @@ table[3, :] = tmep_for_std.min()
 table[4, :] = tmep_for_std.max()
 table[5, :] = np.std(tmep_for_std, axis=0)
 table[6, :] = table[6, :] / times
-tmep_for_loss /= times
-
 table = pd.DataFrame(table)
 table = np.round(table, 2)
 table.columns=name_list
 table.index = ['mean feature selected', 'mean accuracy', 'mean fitness', 'best fitness', 'worst fitness', 'std fitness', 'time']
+
+tmep_for_loss /= times
+tmep_for_loss = pd.DataFrame(tmep_for_loss)
+tmep_for_loss = np.round(tmep_for_loss, 2)
+tmep_for_loss.columns=name_list
